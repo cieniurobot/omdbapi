caad55c3379e4a788790962f522461a5a1cd0421
Marcin Plociennik <mplociennik@trans.eu>
2019-11-23 19:12:34 +0100

    udpate Favourite model

--- src/api_rest/models.py
+++ src/api_rest/models.py
@@ -10,15 +10,15 @@ class Favourite(models.Model):
 
     title = models.CharField(max_length=250, default='Undefined', null=False, blank=False)
     year = models.CharField(max_length=250, default='Undefined', null=False, blank=False)
-    imdbId = models.CharField(max_length=16, default='Undefined', null=False, blank=False)
+    imdb_id = models.CharField(max_length=16, default='Undefined', null=False, blank=False)
     type = models.CharField(choices=TYPE_CHOICES, max_length=7, default='Undefined', null=False, blank=False)
     poster = models.CharField(max_length=250, default='Undefined', null=False, blank=False)
 
     def __str__(self):
-        return "Title: {}: year:{}, imdbId: {}, movie type: {}, poster: {}".format(
+        return "Title: {}: year:{}, imdb_id: {}, movie type: {}, poster: {}".format(
             self.title,
             self.year,
-            self.imdbId,
+            self.imdb_id,
             self.type,
             self.poster
         )
