73e59d597424d2d3f4c11a14819d6dadb435a3f1
Marcin Plociennik <mplociennik@trans.eu>
2019-11-24 12:38:28 +0100

    fav movie views and tests

--- src/api_rest/urls.py
+++ src/api_rest/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^movie/$', views.MovieSearchView.as_view()),
+    url(r'^my-user-favourite-movie/$', views.MyUserFavouriteMovieCreateListView.as_view()),
+    url(r'^my-user-favourite-movie/(?P<pk>[0-9]+)$', views.MyUserFavouriteMovieDeleteView.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
