b28067961a78ca5924091d8961626cc1af17a526
Marcin Plociennik <mplociennik@trans.eu>
2019-11-24 12:37:23 +0100

    update fav movie model

--- src/api_rest/models.py
+++ src/api_rest/models.py
@@ -1,18 +1,24 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
-class Favourite(models.Model):
+class UserFavouriteMovie(models.Model):
     TYPE_CHOICES = [
         ('movie', 'Movie'),
         ('series', 'Series'),
         ('episode', 'Episode'),
     ]
 
-    title = models.CharField(max_length=250, default='Undefined', null=False, blank=False)
-    year = models.CharField(max_length=250, default='Undefined', null=False, blank=False)
-    imdb_id = models.CharField(max_length=16, default='Undefined', null=False, blank=False)
-    type = models.CharField(choices=TYPE_CHOICES, max_length=7, default='Undefined', null=False, blank=False)
-    poster = models.CharField(max_length=250, default='Undefined', null=False, blank=False)
+    title = models.CharField(max_length=250, null=False, blank=False)
+    year = models.CharField(max_length=250, null=False, blank=False)
+    imdb_id = models.CharField(max_length=16, null=False, blank=False)
+    type = models.CharField(choices=TYPE_CHOICES, max_length=7, null=False, blank=False)
+    poster = models.CharField(max_length=250, null=False, blank=False)
+    user = models.ForeignKey(
+        User,
+        related_name='user_favourite_movie',
+        on_delete=models.PROTECT,
+    )
 
     def __str__(self):
         return "Title: {}: year:{}, imdb_id: {}, movie type: {}, poster: {}".format(
